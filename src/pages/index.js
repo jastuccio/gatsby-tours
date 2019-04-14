aec8c13e530fe474996be5e83cda08b0f9e3d6ef
Joe <joe@astucc.io>
2019-04-14 11:03:35 -0400

    feat: Add Generic Navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,10 @@
 import React from "react"
+import styled from "styled-components"
+import Layout from '../components/layout'
+import Hero from "../components/hero"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <Layout>
+    <Hero />
+  </Layout>
+)
